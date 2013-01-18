53af1c2bb2ba059fca75a6b3957ae5bc7eb4d082
Geoff Smith <felixjongleur@gmail.com>
2013-01-18 01:46:11 -0500

    Pass Tests and major refactor.

--- KataBankOCR/src/main/OCRDigits.java
+++ KataBankOCR/src/main/OCRDigits.java
@@ -1,11 +1,68 @@
 package main;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class OCRDigits {
 	
-	public int convert(String digit) {
-		if(digit.equals(" _ | ||_|   "))
-			return 0;
-		else
-			return 1;
+	private final Map<String, Integer> ocrToDigits;
+	
+	public OCRDigits() {
+		ocrToDigits = new HashMap<String, Integer>();
+		ocrToDigits.put(ZERO , 0);
+		ocrToDigits.put(ONE  , 1);
+		ocrToDigits.put(TWO  , 2);
+		ocrToDigits.put(THREE, 3);
+		ocrToDigits.put(FOUR , 4);
+		ocrToDigits.put(FIVE , 5);
+		ocrToDigits.put(SIX  , 6);
+		ocrToDigits.put(SEVEN, 7);
+		ocrToDigits.put(EIGHT, 8);
+		ocrToDigits.put(NINE , 9);
 	}
+	
+	public int convert(String digit) {
+		return ocrToDigits.get(digit);
+	}
+
+
+	static final String ZERO  = " _ " +
+							    "| |" +
+							    "|_|";
+
+	static final String ONE   = "   " +
+							    "  |" +
+							    "  |";
+
+	static final String TWO   = " _ " +
+							    " _|" +
+							    "|_ ";
+
+	static final String THREE = " _ " +
+							    " _|" +
+							    " _|";
+
+	static final String FOUR  = "   " +
+							    "|_|" +
+							    "  |";
+
+	static final String FIVE  = " _ " +
+							    "|_ " +
+							    " _|";
+
+	static final String SIX   = " _ " +
+							    "|_ " +
+							    "|_|";
+
+	static final String SEVEN = " _ " +
+							    "  |" +
+							    "  |";
+
+	static final String EIGHT = " _ " +
+							    "|_|" +
+							    "|_|";
+
+	static final String NINE  = " _ " +
+							    "|_|" +
+							    " _|";
 }
--- KataBankOCR/test/main/OCRDigitsTest.java
+++ KataBankOCR/test/main/OCRDigitsTest.java
@@ -7,30 +7,17 @@ import org.junit.Test;
 public class OCRDigitsTest {
 
 	@Test
-	public void testDigitConverter_Zero() throws Exception {
-		String digit = " _ " +
-					   "| |" +
-					   "|_|" +
-					   "   ";
-		
-		assertEquals(0, new OCRDigits().convert(digit));
+	public void testDigitConverter_Zero() throws Exception {		
+		assertEquals(0, new OCRDigits().convert(OCRDigits.ZERO));
 	}
 	
 	@Test
 	public void testDigitConverter_One() throws Exception {
-		String digit = "   " +
-					   "  |" +
-					   "  |" +
-					   "   ";
-		assertEquals(1, new OCRDigits().convert(digit));
+		assertEquals(1, new OCRDigits().convert(OCRDigits.ONE));
 	}
 	
 	@Test
 	public void testDigitConverter_Nine() throws Exception {
-		String digit = " _ " +
-					   "|_|" +
-					   "  |" +
-					   "   ";
-		assertEquals(9, new OCRDigits().convert(digit));
+		assertEquals(9, new OCRDigits().convert(OCRDigits.NINE));
 	}
 }
