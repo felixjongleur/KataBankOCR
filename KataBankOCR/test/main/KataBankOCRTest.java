19e5eb92523a821d399996b66df8673ea48da825
Geoff Smith <felixjongleur@gmail.com>
2013-01-21 16:02:44 -0500

    Added test file with multiple accounts. Removed unnecessary test files. Created failing test case.

--- KataBankOCR/test/main/KataBankOCRTest.java
+++ KataBankOCR/test/main/KataBankOCRTest.java
@@ -1,5 +1,6 @@
 package main;
 
+import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 
 import java.io.File;
@@ -7,16 +8,12 @@ import java.io.File;
 import org.junit.Test;
 
 public class KataBankOCRTest {
-
-	@Test
-	public void testGetNextAccount() throws Exception {
-		KataBankOCR ocr = new KataBankOCR("/test/main/resources/123456789.txt");
-		assertEquals("123456789", ocr.getNextAccount());
-	}
 	
 	@Test
-	public void testGetNextAccount2() throws Exception {
-		KataBankOCR ocr = new KataBankOCR("/test/main/resources/111111111.txt");
+	public void testGetNextAccount_MultipleRows() throws Exception {
+		KataBankOCR ocr = new KataBankOCR("/test/main/resources/multipleAccounts.txt");
+		assertEquals("123456789", ocr.getNextAccount());
 		assertEquals("111111111", ocr.getNextAccount());
+		assertEquals("664371495 ERR", ocr.getNextAccount());
 	}
 }
